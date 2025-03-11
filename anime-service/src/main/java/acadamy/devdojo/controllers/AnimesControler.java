47f77d0d4c476064237db3ba174fa35cfda70da8
pc-mint <murilonep@gmail.com>
2025-03-11 16:02:57 -0300

    Threads Virtuais, Stacktrace e Tomcat Threads

--- anime-service/src/main/java/acadamy/devdojo/controllers/AnimesControler.java
+++ anime-service/src/main/java/acadamy/devdojo/controllers/AnimesControler.java
@@ -1,5 +1,6 @@
 package acadamy.devdojo.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,21 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("v1/anime")
 @CrossOrigin(origins = "*")
-public class AnimesList {
+@Slf4j
+public class AnimesControler {
 
     @GetMapping()
-    public List<String>  animes() {
+    public List<String>  animes() throws InterruptedException {
         List<String> animes = new ArrayList<>();
 
         animes.add("One Piece");
         animes.add("Bakugan");
         animes.add("Bleach");
         animes.add("Naruto");
-
+        log.info(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(1);
         return animes;
     }
 
